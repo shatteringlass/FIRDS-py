6e6afd470a14a6e94ef5895b998abd1dad812553
shatteringlass <federico.pizzolo@gmail.com>
2019-06-03 18:39:40 +0200

    FIXME: json data passed to post request is broken

--- firds/firds.py
+++ firds/firds.py
@@ -11,58 +11,86 @@ from firds.util.misc import retry
 
 
 class CFI(enum.Enum):
-    EQUITY = 'E'
-    DEBT = 'D'
-    COLLECTIVE_INVESTMENT_VEHICLES = 'C'
-    ENTITLEMENTS = 'R'
-    LISTED_OPTIONS = 'O'
-    FUTURES = 'F'
-    SWAPS = 'S'
-    NON_LISTED_OPTIONS = 'H'
-    SPOT = 'I'
-    FORWARDS = 'J'
-    STRATEGIES = 'K'
-    REFERENCE_INSTRUMENTS = 'T'
-    OTHERS = 'M'
+    EQUITY = "E"
+    DEBT = "D"
+    COLLECTIVE_INVESTMENT_VEHICLES = "C"
+    ENTITLEMENTS = "R"
+    LISTED_OPTIONS = "O"
+    FUTURES = "F"
+    SWAPS = "S"
+    NON_LISTED_OPTIONS = "H"
+    SPOT = "I"
+    FORWARDS = "J"
+    STRATEGIES = "K"
+    REFERENCE_INSTRUMENTS = "T"
+    OTHERS = "M"
+
+
+class FirdsEntry:
+
+    __slots__ = ["_root_", "_version_", "collectorpaRent", "drv_delivery_type", "drv_expiry_date",
+                 "drv_price_multiplier", "drv_sp_prc_value_sign_flag", "drvcmd_base_product", "drvcmd_final_price_type",
+                 "drvcmd_sub_product", "drvcmd_transaction_type", "gnr_cfi_code", "gnr_comm_derivative_flag",
+                 "gnr_full_name", "gnr_notional_curr_code", "gnr_short_name", "id", "isin", "latest_received_flag",
+                 "lei", "mic", "mrkt_issr_trdng_pprvl_date", "mrkt_issr_trdng_rqst_flag", "mrkt_trdng_rqst_date",
+                 "mrkt_trdng_start_date", "mrkt_trdng_trmination_date", "never_published_flag", "publication_date",
+                 "published_from_date", "published_to_date", "type_s", "upcoming_rca"]
+
+    def __init__(self, *args, **kwargs):
+        for field in self.__slots__:
+            self.setattr(field, kwargs.get(field))
 
 
 class FirdsQuery:
 
-    search_endpoint = 'https://registers.esma.europa.eu/publication/searchRegister/doMainSearch'
+    UA = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    URL = "https://registers.esma.europa.eu"
+    search_register = "/publication/searchRegister"
+    search_endpoint = "/doMainSearch"
+    service = "esma_registers_firds"
 
-    headers = {
-        'Origin': 'https://registers.esma.europa.eu',
-        'Referer': 'https://registers.esma.europa.eu/publication/searchRegister?core=esma_registers_firds',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Connection': 'keep-alive',
-    }
+    def __init__(self):
+        self.headers = {
+            "Origin": self.URL,
+            "Referer": f"{self.URL}{self.search_register}?core={self.service}",
+            "Accept-Encoding": "gzip, deflate, br",
+            "User-Agent": self.UA,
+            "Content-Type": "application/json",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "X-Requested-With": "XMLHttpRequest",
+            "Connection": "keep-alive",
+        }
 
-    def build_query(self, start=0, criteria=list()):
-        return json.dumps({
-            "core": "esma_registers_firds",
-            "pagingSize": "1000",
-            "start": start,
+    def build_query(self, paging_size=1000, start=0, criteria=list()):
+        criteria.append({
+            "name": "firdsPublicationDateCustomSearchInputField",
+            "value": "(latest_received_flag:1)AND(never_published_flag:0)",
+            "type": "customSearchInputFieldQuery",
+            "isParent": "true"
+        })
+        return {
+            "core": self.service,
+            "pagingSize": str(paging_size),
+            "start": str(start),
             "keyword": "",
             "sortField": "isin asc",
             "criteria": criteria,
             "wt": "json"
-        })
+        }
 
     def get(self, criteria):
-        query = self.build_query(criteria=criteria)
-        return requests.post(self.search_endpoint,
+        f = [dict(name=k, value=v, type="text") for k, v in criteria.items()]
+        query = json.dumps(self.build_query(criteria=f))
+        endpoint = self.URL+self.search_register+self.search_endpoint
+        return requests.post(endpoint,
                              headers=self.headers,
-                             data=query
+                             json=query
                              ).json()
 
 
 class FirdsDB:
 
-    URL = 'https://registers.esma.europa.eu/solr/esma_registers_firds_files/select'
+    URL = "https://registers.esma.europa.eu/solr/esma_registers_firds_files/select"
     today = datetime.date.today()
     amnight = datetime.datetime.min.time()
     bmnight = datetime.datetime.max.time()
@@ -84,7 +112,7 @@ class FirdsDB:
 
     @classmethod
     def zulu_dt(cls, dt):
-        return dt.isoformat().replace('+00:00', 'Z')
+        return dt.isoformat().replace("+00:00", "Z")
 
     @classmethod
     def calc_start_date(cls, end_date):
@@ -101,13 +129,13 @@ class FirdsDB:
     def base_request(self, *args, **kwargs):
 
         base_params = {
-            'q': '*',  # tells the response to return all columns for a given result if one exists
+            "q": "*",  # tells the response to return all columns for a given result if one exists
             # publication date interval
-            'fq': f"publication_date:[{kwargs.get('start_date')} TO {kwargs.get('end_date')}]",
-            'wt': 'json',  # xml / json format of response
-            'indent': True,  # make response more readable
-            'start': kwargs.get('start'),  # response offset
-            'rows': kwargs.get('max_rows')
+            "fq": f"publication_date:[{kwargs.get('start_date')} TO {kwargs.get('end_date')}]",
+            "wt": "json",  # xml / json format of response
+            "indent": True,  # make response more readable
+            "start": kwargs.get("start"),  # response offset
+            "rows": kwargs.get("max_rows")
         }
         response = self.session.get(
             url=self.URL, params=base_params, proxies=self.proxies)
@@ -127,17 +155,17 @@ class FirdsDB:
 
         response = self.base_request(
             start_date=dt_from, end_date=dt_to, start=start, max_rows=max_rows).json()
-        num_found = response['response']['numFound']
+        num_found = response["response"]["numFound"]
 
-        jpath = jsonpath_rw.parse('response.docs[*].download_link')
-        prd_re = re.compile('^.*?_([A-Z]{1})_.*?$')
+        jpath = jsonpath_rw.parse("response.docs[*].download_link")
+        prd_re = re.compile("^.*?_([A-Z]{1})_.*?$")
 
         for k in jpath.find(response):
-            if 'FULINS' in k.value:
+            if "FULINS" in k.value:
                 p = CFI(re.match(prd_re, k.value).group(1)).name
-                lst['FULINS'].setdefault(p, []).append(k.value)
-            elif 'DLTINS' in k.value:
-                lst.setdefault('DLTINS', []).append(k.value)
+                lst["FULINS"].setdefault(p, []).append(k.value)
+            elif "DLTINS" in k.value:
+                lst.setdefault("DLTINS", []).append(k.value)
             else:
                 # unhandled file type
                 continue
@@ -154,28 +182,28 @@ class FirdsDB:
             """
             Get filename from content-disposition
             """
-            cd = r.headers.get('content-disposition')
+            cd = r.headers.get("content-disposition")
             if not cd:
                 return None
-            fname = re.findall('filename=(.+)', cd)
+            fname = re.findall("filename=(.+)", cd)
             if len(fname) == 0:
                 return None
             return fname[0]
 
         response = requests.get(link, stream=True)
-        fname = get_filename_from_cd(response) or link.rsplit('/', 1)[1]
+        fname = get_filename_from_cd(response) or link.rsplit("/", 1)[1]
         # Throw an error for bad status codes
         response.raise_for_status()
 
         # Write chunks to file
         path = os.path.join(self.tmpfolder.name, fname)
-        with open(path, 'wb') as handle:
+        with open(path, "wb") as handle:
             for block in response.iter_content(1024):
                 handle.write(block)
         return path
 
     def download_delta(self):
-        for l in self.links['DLTINS']:
+        for l in self.links["DLTINS"]:
             print(self.get_zip(l))
 
     def download_all(self):
--- test.py
+++ test.py
@@ -13,39 +13,10 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-criteria = [
-    {
-        "name": "gnr_full_name",
-                "value": "PSV",
-                "type": "text",
-                "isParent":
-                    "true"
-    },
-    {
-        "name": "mic",
-                "value": "NDEX",
-                "type": "text",
-                "isParent": 'true'
-    },
-    {
-        "name": "gnr_cfi_code",
-                "value": "*",
-                "type": "text",
-                "isParent": 'true'
-    },
-    {
-        "name": "firdsPublicationDateCustomSearchInputField",
-                "value": "(latest_received_flag:1)AND(never_published_flag:0)",
-                "type": "customSearchInputFieldQuery",
-                "isParent": 'true'
-    }
-]
-
-
-
 
 def main():
-    query = FirdsQuery().get(criteria=criteria)
+    k = {'gnr_full_name':'PSV', 'mic': 'NDEX', 'gnr_cfi_code': '*'}
+    query = FirdsQuery().get(criteria=k)
     json.dump(query, fp=sys.stdout, indent=4)
     # json.dump(f.links, fp=sys.stdout, indent=True)
 
